0f5b1d4fb439beb697a874f39e4c91cffac8e25a
Hsuan-Hau Liu <hsuanhauliu@gmail.com>
2020-06-14 20:49:00 -0700

    chore: display random image if no input image given

--- super_resolution/visualize_result.py
+++ super_resolution/visualize_result.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+from random import choice
 
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -8,19 +10,38 @@ from inference import inference
 
 def main(opt):
     """ main function """
-    input_image = Image.open(opt.input_image)
-    output_img = inference(input_image, opt.model, cuda=opt.cuda)
+    print(opt.input_image)
+    if opt.input_image:
+        visualize(opt.input_image, opt.model, opt.cuda)
+    else:
+        test_img_dir = 'dataset/BSDS300/images/test/'
+        train_img_dir = 'dataset/BSDS300/images/train/'
+        test_images = os.listdir(test_img_dir)
+        train_images = os.listdir(train_img_dir)
+        all_images = [test_img_dir + img for img in test_images] + [train_img_dir + img for img in train_images]
+
+        # Ctrl + c to stop
+        while True:
+            input_image = choice(all_images)
+            visualize(input_image, opt.model, opt.cuda)
+        
+
+def visualize(input_image, model, cuda):
+    input_image = Image.open(input_image)
+    output_image = inference(input_image, model, cuda=cuda)
+    print(f'input image size: {input_image.size}')
+    print(f'output image size: {output_image.size}')
 
     f, axarr = plt.subplots(1, 2)
     axarr[0].imshow(input_image)
-    axarr[1].imshow(output_img)
+    axarr[1].imshow(output_image)
     plt.show()
 
 
 if __name__ == '__main__':
     # Training settings
     parser = argparse.ArgumentParser(description='Compare input and output result')
-    parser.add_argument('--input_image', type=str, required=True, help='input image to use')
+    parser.add_argument('--input_image', type=str, default='', help='input image to use')
     parser.add_argument('--model', type=str, required=True, help='model file to use')
     parser.add_argument('--cuda', action='store_true', help='use cuda')
     opt = parser.parse_args()
